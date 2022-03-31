fdee945543165ccb827e804c072aaa6b46308bb4
arif0ne <ayanarif2001@gmail.com>
2022-03-31 23:17:53 +0600

    this facebook clone website ui is complete but some kind of javascript logic are working and one bugs try to fix

--- js/main.js
+++ js/main.js
@@ -1,1 +1,56 @@
-console.log('hello world');
+// --------------Sidebar---------------
+// -----------Variables declire----------
+const menuItems = document.querySelectorAll(".menu-item");
+const messagesNotification = document.querySelector("#messages-notifications");
+const messages = document.querySelector(".messages");
+const message = messages.querySelectorAll(".message")
+const messageSearch = document.querySelector("#messages-search");
+
+
+// Remove active class when active one click class 
+const changeActiveItem = () => {
+    menuItems.forEach(item => {
+        item.classList.remove("active");
+    });
+}
+
+menuItems.forEach(item => {
+    item.addEventListener("click", () => {
+        changeActiveItem();
+        item.classList.add("active");
+        if (item.id != "notifications") {
+            document.querySelector(".notifications-popup").style.display = "none";
+        } else {
+            document.querySelector(".notifications-popup").style.display = "block";
+            document.querySelector("#notifications .notification-count").style.display = "none";
+        }
+    });
+});
+
+
+// --------------Messages---------------
+
+// Search Chat 
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase();
+    message.forEach(chat => {
+        let name = chat.querySelectorAll('h5').textContent.toLowerCase();
+        if (name.indexOf(val) != -1) {
+            chat.style.display = "flex";
+        } else {
+            chat.style.display = "none";
+        }
+    });
+}
+
+//Search Message
+messageSearch.addEventListener("keyup", searchMessage);
+
+// hightlight message when clicked the message 
+messagesNotification.addEventListener("click", () => {
+    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    messagesNotification.querySelector(".notification-count").style.display = "none";
+    setTimeout(() => {
+        messages.style.boxShadow = "none";
+    }, 2000);
+});
